c0513868230c899950e8afdd6ae8dd7fb99147a1
Javier Honduvilla Coto <javierhonduco@gmail.com>
2014-12-06 17:19:26 +0100

    Update cartodb_sql.py

--- cartodb_sql.py
+++ cartodb_sql.py
@@ -10,3 +10,15 @@ def location_intersects(lat, lon):
     results = cl.sql('select count(*) from bboxes where st_intersects(CDB_LatLng(%f, %f), the_geom)' %(lat,lon))
     return results['rows'][0]['count']
 
+def dummy_create_company(data):
+    SQL = '''
+            INSERT INTO company 
+	            (email, password, company_name, 
+	                phone, geolocation_methods, 
+                    enabled_browsers, operating_systems)
+            VALUES ('{email}', '{password}', '{company_name}', 
+       	        '{phone}', {geolocation_methods}, {enabled_browsers}, {operating_systems})'''
+    query = SQL.format(*data)
+    results = cl.sql(query)
+    return results
+    
