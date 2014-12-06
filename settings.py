d688268cce78a35277930681695fe881259dc3e4
Jorge Arévalo <jorge@cartodb.com>
2014-12-06 21:50:09 +0100

    Added login capabilities and some basic template. The config template is not still used

--- settings.py
+++ settings.py
@@ -4,4 +4,7 @@ PORT = 5000
 MANDRILL_API_KEY = '1nxQ4TNkhBPb3OTu8O9kiQ'
 MANDRILL_DEFAULT_FROM = 'loglock@loglock.ninja'
 
+#This is totally random
+SECRET_KEY='sxa2XXfBJjEsFnce6NvrAMLh '
+
 CORS_HEADERS = 'Content-Type'
